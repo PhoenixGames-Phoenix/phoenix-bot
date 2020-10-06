49c6267c8c59deeada325f34974f0610df3c6684
Phoenix_13 <47122082+PhoenixGames-Phoenix@users.noreply.github.com>
2020-10-06 21:13:37 +0200

    Added reload and config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # phoenix-bot
-The Discord bot for my personal public discord server 
+
+The Discord bot for my personal public discord server
+
+![GitHub](https://img.shields.io/github/license/PhoenixGames-Phoenix/phoenix-bot)
+[![CodeFactor](https://www.codefactor.io/repository/github/phoenixgames-phoenix/phoenix-bot/badge)](https://www.codefactor.io/repository/github/phoenixgames-phoenix/phoenix-bot)
+![GitHub Workflow Status (branch)](https://img.shields.io/github/workflow/status/PhoenixGames-Phoenix/phoenix-bot/phoenix-bot%20eslint%20CI/master?label=ESLint%20CI)
+![GitHub Workflow Status (branch)](https://img.shields.io/github/workflow/status/PhoenixGames-Phoenix/phoenix-bot/phoenix-bot%20docker%20CI/master?label=Docker%20Build)
---
+++ src/commands/reload.js
@@ -0,0 +1,26 @@
+const config = require('../config.js');
+
+module.exports = {
+    name: 'reload',
+    description: 'Reloads the specified command. Can only be executed by the bot owner',
+    usage: 'reload <command>',
+    async execute(message, args) {
+        if (message.author.tag != config.botowner.toString()) return await message.channel.send(config.messages.unauthorized);
+        if (!args.length) return await message.channel.send(config.messages.notenoughargs);
+        const commandName = args[0].toLowerCase();
+        const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+        if(!command) return await message.channel.send(config.messages.unknwoncommand);
+
+        delete require.cache[require.resolve(`./${command.name}.js`)];
+
+        try {
+            const newCommand = require(`./${command.name}.js`);
+            await message.client.commands.set(newCommand.name, newCommand);
+            await message.channel.send(`Successfuly reloaded command ${commandName}`);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    } 
+}
